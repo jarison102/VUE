8fd15cc00ca2adbab416cb533dcc786b7c21d79f
Jarison <Jarimices@gmail.com>
2023-07-26 20:51:40 -0500

    Arrays

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,20 @@ const Name="Vue Dinamico";
 
 var Nombre = "Jarison";
 if(Nombre === "Jarison"){
-   alert("Hola Soy Jarison")
+   alert("Hola Soy Jarison");
 }else{
-   alert ("No es Jarison")
+   alert ("No es Jarison");
 }
 
+const Names = ["Jarison","STIVED","HU","YI","YASUO","VUE","JAVASCRIPT"];
 </script>
 
 
 <template>
   <h1>H1 {{ Name }}</h1>
+  <ul>
+    <li v-for="(Nombres ,index) in Names ">{{ index }} - {{ Names }}</li>
+  </ul>
 </template>
 
 <style>
